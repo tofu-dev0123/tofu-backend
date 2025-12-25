24f7a14fc9ec9ed3c81533f229551848258c641d
tofu-dev0123 <m.komukai0123@gmail.com>
2025-12-25 10:11:01 +0900

    corsミドルウェアの追加

--- app/main.py
+++ app/main.py
@@ -17,6 +17,15 @@ logging.basicConfig(
 app = FastAPI()
 register_exception_handlers(app)
 
+# CORS設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ALLOW_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allow_headers=["*"],
+)
+
 # ルーターの登録
 app.include_router(api_router)
 
